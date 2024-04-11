4793a3bc22a85e29739b499329a22f6a37d90918
Jammi Dee <jammi_dee@yahoo.com>
2024-04-11 19:18:39 +0800

    Initial commit Lalulla Framework Spring-boot

---
+++ src/test/java/com/lalulla/lakubosb/LakubosbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalulla.lakubosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakubosbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
