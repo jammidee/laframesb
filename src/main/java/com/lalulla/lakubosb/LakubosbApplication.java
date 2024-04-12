dbe4ba210b8ec6b41a2f805bfd17290e1e827f08
Jammi Dee <jammi_dee@yahoo.com>
2024-04-12 11:11:48 +0800

    Added color console display using printf.

--- src/main/java/com/lalulla/lakubosb/LakubosbApplication.java
+++ src/main/java/com/lalulla/lakubosb/LakubosbApplication.java
@@ -39,7 +39,7 @@ public class LakubosbApplication {
     private static final String ANSI_COLOR_RESET 	= "\u001B[0m";
     //private static final String ANSI_COLOR_RED 		= "\u001B[31m";
     private static final String ANSI_COLOR_GREEN 	= "\u001B[32m";
-    //private static final String ANSI_COLOR_YELLOW 	= "\u001B[33m";
+    private static final String ANSI_COLOR_YELLOW 	= "\u001B[33m";
 
 	public static void main(String[] args) {
 		SpringApplication.run(LakubosbApplication.class, args);
@@ -50,12 +50,13 @@ public class LakubosbApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
-			System.out.printf("\n\n%sLet's inspect the beans provided by Spring Boot:%s\n\n", ANSI_COLOR_GREEN, ANSI_COLOR_RESET );
+			System.out.printf("\n\n%sLet's inspect the beans provided by Spring Boot:%s\n\n", ANSI_COLOR_GREEN, ANSI_COLOR_RESET);
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
-				System.out.println(beanName);
+				//System.out.println(beanName);
+				System.out.printf(" Bean: %s%s%s\n", ANSI_COLOR_YELLOW, beanName, ANSI_COLOR_RESET );
 			}
 
 		};
